18217e91cf29d57dcbfe27d43080013e10d17e39
Brand0nLe <93brandonle@gmail.com>
2023-06-13 11:33:22 -0700

    re did user form component

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.css']
 })
-export class UserFormComponent {
+export class UserFormComponent implements OnInit{
+  userForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(){
+    // this.userForm = new FormGroup({
+    //   fName: new FormControl('', Validators.required),
+    //   lName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    //   mName: new FormControl('', Validators.required),
+    //   hasMiddleName: new FormControl(false)
+      
+    // });
+
+    this.userForm = this.formBuilder.group({
+      fName: ['', Validators.required],
+      lName: ['', [Validators.required, Validators.minLength(2)]],
+      mName: ['', Validators.required],
+      hasMiddleName: false
+    });
+  }
 
 }
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserFormComponent } from '../user-form/user-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { UserFormComponentComponent } from './user-form-component/user-form-component.component';
 
 
 @NgModule({
   declarations: [ 
-    UserFormComponent, UserFormComponentComponent,
+    UserFormComponent,
    ],
   imports: [
     CommonModule,
